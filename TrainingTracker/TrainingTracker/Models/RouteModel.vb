3879ed126e4c389c323fb57e9f494bc773126613
Linnea Grenquist <grenquistl0915@my.uwstout.edu>
2015-04-30 00:08:03 -0500

    Fixed Route Distance pull

--- TrainingTracker/TrainingTracker/Models/RouteModel.vb
+++ TrainingTracker/TrainingTracker/Models/RouteModel.vb
@@ -2,7 +2,7 @@
     Public Property IsActive As Boolean
     Public Property RouteName As String
     Public Property Value As String
-    Public Property Distance As Double
+    Public Property Distance As Single
     Public Property CheckboxOnly As Boolean
     Public Property CheckBoxValue As Boolean
 End Class
